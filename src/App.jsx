c8981f350e4a68b92bd5de37373e4bc780858c00
cinos81 <cinos81@gmail.com>
2023-08-20 16:20:14 +0900

    1차구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const inputRef = useRef();
+  const onClick = () => {
+    setTodoList([...todoList, inputRef.current.value]);
+  };
+  const onKeyUp = (e) => {
+    if (e.keyCode === 13) {
+      setTodoList([...todoList, inputRef.current.value]);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center w-screen h-screen font-medium">
       <div className="flex flex-grow items-center justify-center bg-gray-900 h-full">
@@ -23,50 +35,67 @@ function App() {
             </svg>
             <h4 className="font-semibold ml-3 text-lg">Todo List</h4>
           </div>
-          <div>
-            <input className="hidden" type="checkbox" id="task" />
-            <label
-              className="flex items-center h-10 px-2 rounded cursor-pointer hover:bg-gray-900"
-              htmlFor="task"
-            >
-              <span className="flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-500 rounded-full">
-                <svg
-                  className="w-4 h-4 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
+          {/* todo[s] */}
+          {todoList.map((todo, index) => {
+            const key = `${todo}_${index}`;
+
+            return (
+              <div key={key}>
+                <input className="hidden" type="checkbox" id="task" />
+                <label
+                  className="flex items-center h-10 px-2 rounded cursor-pointer hover:bg-gray-900"
+                  htmlFor="task"
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </span>
-              <span className="select-none ml-4 text-sm">Trim the verge.</span>
-            </label>
-          </div>
-          <button className="flex items-center w-full h-8 px-2 mt-2 text-sm font-medium rounded">
-            <svg
-              className="w-5 h-5 text-gray-400 fill-current"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+                  <span className="flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-500 rounded-full">
+                    <svg
+                      className="w-4 h-4 fill-current"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                  <span className="select-none ml-4 text-sm">{todo}</span>
+                </label>
+              </div>
+            );
+          })}
+          {/* todo[e] */}
+          {/* form[s] */}
+          <div className="flex items-center w-full mt-2">
+            <button
+              className="h-8 px-2 text-sm font-medium rounded"
+              onClick={onClick}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-              />
-            </svg>
+              <svg
+                className="w-5 h-5 text-gray-400 fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                />
+              </svg>
+            </button>
             <input
               className="flex-grow h-8 ml-4 bg-transparent focus:outline-none font-medium"
               type="text"
               placeholder="add a new task"
+              ref={inputRef}
+              onKeyUp={onKeyUp}
             />
-          </button>
+          </div>
+          {/* form[e] */}
         </div>
         {/* Component End  */}
       </div>
