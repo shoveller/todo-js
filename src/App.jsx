9a99bd2e493c6e8fae71d09d7fc5dd92e3d18adf
서재원 <cinos81@nhnlink.co.kr>
2023-08-22 14:33:32 +0900

    읽기기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -29,20 +29,18 @@ function App() {
             <h4 className="font-semibold ml-3 text-lg">Todo List</h4>
           </div>
           {
-            list.map((task, index) => {
-              const key = `task_${index}`;
-
+            list.map(({ id, todo, completed }) => {
               return (
-                <div className="group" key={key}>
+                <div className="group" key={id}>
                   <label className="flex items-center h-10 px-2 rounded cursor-pointer hover:bg-gray-900">
-                    <input name="hidden-check" className="hidden peer" type="checkbox" />
+                    <input name="hidden-check" className="hidden peer" type="checkbox" defaultChecked={completed} />
                     <span name="check" className="peer-checked:bg-green-500 peer-checked:border-green-500 peer-checked:text-white flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-500 rounded-full">
                       <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                       </svg>
                     </span>
                     <span className="peer-checked:text-gray-500 peer-checked:line-through select-none mx-4 text-sm w-full" name="viewer">
-                      Trim the verge.
+                      {todo}
                     </span>
                     <button className="hidden group-hover:block">
                       <svg className="w-5 h-5 text-gray-400 fill-current mx-1" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 27.965 27.965" xmlSpace="preserve">
